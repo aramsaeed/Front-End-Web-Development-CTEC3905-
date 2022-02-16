2e660cf1f0697436c1c7559dfae757e804626536
Aram Saeed <51077491+aramsaeed@users.noreply.github.com>
2022-02-16 10:44:03 +0000

    I add script

---
+++ script/script.js
@@ -0,0 +1,4 @@
+function Para_Func()
+{
+	document.getElementById("target").innerHTML = "The Internet of Things (IoT) is the term which refers to the ever-growing network of physical objects with embedded sensors which can connect together via the internet allowing communication to occur between these objects and many other Internet-enabled devices and systems.";
+}
